2d87a07546abea658d9ea00fb93c51831421d42e
Boaz Sze <boaz.sze@gmail.com>
2019-05-30 00:11:36 +0800

    Test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 use App\StreamChunk;
 use App\Observers\StreamChunkObserver;
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         StreamChunk::observe(StreamChunkObserver::class);
     }
 
